93e59076b3cc0fb526a3c90fcf1b577ad3d3315d
pandanon50 <60589856+pandanon50@users.noreply.github.com>
2020-11-14 17:00:12 +0900

    database post,user,posts

---
+++ routes/posts.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const moment = require("moment");
+const router = express.Router();
+const { Post, User } = require("../models");
+const { isLoggedIn } = require("./middlewares");
+
+const today = moment();
+console.log(today);
+const value = today.format("YYYYMMDD");
+console.log(value);
+
+router.get("/", isLoggedIn, async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      where: {
+        date: value,
+        UserId: req.user.id,
+      },
+    });
+    res.status(201).json(posts);
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
