e38eb9ed28ecdcde8d4d37f619bcd00e1c8130e0
pandanon50 <60589856+pandanon50@users.noreply.github.com>
2020-11-23 17:41:52 +0900

    fix page access permission

--- components/Footer.js
+++ components/Footer.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { initMic } from "../Speech/micSet";
-import { Modal } from "antd";
+import { message, Modal } from "antd";
 import Link from "next/link";
 import {
   ADD_POST_REQUEST,
@@ -13,26 +13,37 @@ import { UpdateSpeech } from "../Speech/speechAction";
 import { ret, recognition, clearRet } from "../Speech/speechStart";
 import { HomeOutlined, AudioOutlined, UserOutlined } from "@ant-design/icons";
 import { SpeechText } from "../Speech/Text2Speech";
+import Router from "next/router";
 
 const Footer = () => {
   const dispatch = useDispatch();
-  const { monthTodos, findItem } = useSelector((state) => state.post);
+  const { me } = useSelector((state) => state.user);
+  const { todos, monthTodos, findItem } = useSelector((state) => state.post);
   const [visible, setVisible] = useState(false);
   const [str, setStr] = useState("");
 
-  const handleOk = () => {
+  const handleOk = async () => {
     setVisible(false);
     const e = UpdateSpeech(ret);
     // console.log(UpdateSpeech(ret));
-    dispatch(e);
+    await dispatch(e);
     // 모든 today 읽어주기
     if (e.type === LOAD_DATE_POST_REQUEST) {
-      console.log(monthTodos);
-      let a = monthTodos.length;
-      for (let i = 0; i < a; i++) {
-        SpeechText(monthTodos[i].content);
+      if (todos !== null && monthTodos === null) {
+        let a = todos.length;
+        for (let i = 0; i < a; i++) {
+          SpeechText(todos[i].content);
+        }
+        SpeechText("일정이 있습니다.");
+      } else if (todos === null && monthTodos !== null) {
+        let a = monthTodos.length;
+        for (let i = 0; i < a; i++) {
+          SpeechText(monthTodos[i].content);
+        }
+        SpeechText("일정이 있습니다.");
+      } else {
+        SpeechText("일정이 없습니다.");
       }
-      SpeechText("일정이 있습니다.");
     }
     setStr("");
     clearRet();
@@ -49,8 +60,13 @@ const Footer = () => {
   };
 
   const showModal = () => {
-    setVisible(true);
-    initMic();
+    if (!me) {
+      message.error("로그인이 필요합니다.");
+      Router.replace("/user");
+    } else {
+      setVisible(true);
+      initMic();
+    }
   };
 
   if (typeof window !== "undefined") {
--- components/GoalItem.js
+++ components/GoalItem.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useCallback } from "react";
 import PropTypes from "prop-types";
 import { Collapse } from "antd";
+import { DeleteOutlined } from "@ant-design/icons";
 import SuccessButton from "./SuccessButton";
 import DoneButton from "./DoneButton";
 import styled, { createGlobalStyle } from "styled-components";
@@ -9,8 +10,8 @@ const { Panel } = Collapse;
 
 const GridDiv = styled.div`
   display: grid;
-  grid-template-columns: repeat(12, 3fr);
-  grid-template-rows: repeat(12, 5fr);
+  grid-template-columns: repeat(9, 1fr);
+  grid-template-rows: repeat(4, 1fr);
   grid-gap: 3px;
 `;
 
@@ -23,6 +24,11 @@ const Global = createGlobalStyle`
 const GoalItem = ({ goal }) => {
   const { goalTitle, checkTotal, checkDone, id, startLine, endLine } = goal;
   console.log(goalTitle, checkTotal, id, startLine, endLine);
+
+  const onClickRemove = useCallback(() => {
+    console.log("hi");
+  }, []);
+
   // const Icon = () => {
   //   return "👉";
   // };
@@ -31,8 +37,22 @@ const GoalItem = ({ goal }) => {
       <Global />
       <Collapse defaultActiveKey={["1"]} ghost>
         <Panel header={goalTitle} key={id}>
-          <div>시작일:{startLine}</div>
-          <div>끝나는일:{endLine}</div>
+          <div
+            style={{
+              marginBottom: "5px",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div>
+              <div>시작일: {startLine}</div>
+              <div>끝나는일: {endLine}</div>
+            </div>
+            <div style={{ fontSize: "20px" }}>
+              <DeleteOutlined onClick={onClickRemove} />
+            </div>
+          </div>
           <GridDiv>
             {checkDone !== 0 &&
               [...Array(checkDone)].map((i) => (
